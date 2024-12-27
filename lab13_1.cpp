68330e4578b9eab7a59d7939cd5cd15c2ef58f33
Perm <wannuwatbuayangyuen@gmail.com>
2024-12-27 16:22:37 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,38 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0;
+    double mean = 0;
+    double SD = 0;
+    double GM = 1;
+    double HM = 0;
+    double Min = A[0];
+    double Max = A[0];
+
+    for(int i = 0; i < N;i++){
+      sum += A[i];     
+      GM *= A[i];
+      HM += 1/A[i];
+      mean = sum/N;
+    }
+    for(int i = 0; i < N;i++){
+        SD += pow((A[i]-mean),2)/N;
+    }
+    for(int i = 0; i < N;i++){
+        if(Max < A[i]){
+        Max = A[i];
+      }
+      if(Min > A[i]){
+        Min = A[i];
+      }
+    }
+
+    B[0] = mean;
+    B[1] = sqrt(SD);
+    B[2] = pow(GM,1.00/N);
+    B[3] = N/HM;
+    B[4] = Max;
+    B[5] = Min;
+}
